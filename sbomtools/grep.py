37ffe35a0e73f0d26d38f013cf5fb0b72964f8fd
Eliot Lear <lear@cisco.com>
2022-01-31 12:14:30 +0100

    A bit more correcting for lint.

--- sbomtools/grep.py
+++ sbomtools/grep.py
@@ -7,7 +7,7 @@ Routines to search SBOMs.
 """
 
 import json
-from sbomtools.constants import *
+from sbomtools.constants import SEEN_CDX,SEEN_SPDX
 from sbomtools.cyclonedx import cyclonedx_search
 from sbomtools.spdx import spdx_search
 
@@ -20,17 +20,16 @@ def sbom_grep(filename,search_fp,searchstr, want_json = True):
     sbom=json.loads(search_fp.read())
 
     if 'bomFormat' in sbom and sbom['bomFormat'] == 'CycloneDX':
-        res= (SEEN_CDX,cyclonedx_search(searchstr,sbom,want_json))
+        (state,res)= (SEEN_CDX,cyclonedx_search(searchstr,sbom,want_json))
     if 'spdxVersion' in sbom:
-        res= (SEEN_SPDX,spdx_search(searchstr,sbom,want_json))
+        (state,res)= (SEEN_SPDX,spdx_search(searchstr,sbom,want_json))
     if not res:
         return False
 
-    output=''
-
     if want_json:
-        return res
-    (state,text) = res
-    for entry in text:
+        return (state,res)
+
+    output=''
+    for entry in res:
         output=output + f'{filename}: {entry["name"]} version {entry["version"]}\n'
     return output
--- sbomtools/sbomgrep.py
+++ sbomtools/sbomgrep.py
@@ -9,7 +9,7 @@ Provide CLI functionality for sbomgrep
 import argparse
 import json
 from sys import stdin
-from sbomtools.constants import *
+from sbomtools.constants import SEEN_NONE
 from sbomtools.grep import sbom_grep
 
 def print_json_results(args):
@@ -29,7 +29,7 @@ def print_json_results(args):
         try:
             with open(filename,'r',encoding='utf8') as s_fp:
                 (state,output)=sbom_grep(filename,s_fp,args.searchstr[0],args.json)
-                if sbom_type != state and sbom_type != SEEN_NONE:
+                if sbom_type not in ( state, SEEN_NONE ):
                     print(f'{filename}: mixed SBOM types in JSON')
                     return
                 sbom_type=state
